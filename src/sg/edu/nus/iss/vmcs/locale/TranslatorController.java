36d1d639bbb7f18d1ecf4f5bb25c405f251db977
Oscar <oscaraya@gmail.com>
2017-04-26 00:00:31 +0800

    Added Pirate

--- src/sg/edu/nus/iss/vmcs/locale/TranslatorController.java
+++ src/sg/edu/nus/iss/vmcs/locale/TranslatorController.java
@@ -8,6 +8,7 @@ import sg.edu.nus.iss.vmcs.system.MainController;
 public class TranslatorController {
 	public static final String SPANISH = "Spanish";
 	public static final String ENGLISH = "English";
+	public static final String PIRATE = "Pirate";
 	
 	/**This attribute reference to the MainController*/
 	public MainController mainCtrl;
@@ -18,6 +19,8 @@ public class TranslatorController {
 		
 		if (language.compareTo(SPANISH) == 0){
 			Locale.setDefault(new Locale("sp", "CR"));
+		}else if(language.equals(PIRATE)){
+			Locale.setDefault(new Locale("en", "PI"));
 		}else{
 			Locale.setDefault(new Locale("en", "SG"));
 		}
